9c81d633ac8ab1d23c7cafa47215a470e92c8c0f
Haoran Wang <wanghaoran19199@outlook.com>
2025-10-06 05:59:42 +0800

    added VFX for destroyed collector

--- Assets/!workspace/wanghaoran19199/Scripts/CollectorDisableStateControl_TypeAlpha.cs
+++ Assets/!workspace/wanghaoran19199/Scripts/CollectorDisableStateControl_TypeAlpha.cs
@@ -1,6 +1,43 @@
+using System;
 using UnityEngine;
 
 public class CollectorDisableStateControl_TypeAlpha : MonoBehaviour
 {
-    [SerializeField] private CollectorDestroyableComponents[] locomotionObjects;
+    [SerializeField] private CollectorDestroyableComponent[] locomotionObjects;
+    [SerializeField] private CollectorDestroyableComponent[] fuelTankObjects;
+    [SerializeField] private CollectorDestroyableComponent computerRoomObject;
+    [SerializeField] private CollectorDestroyableComponent cargoHoldObject;
+
+    private bool _isDisabled, _isFullyDestroyed, _canReceiveRewards = true;
+    
+
+    private void Update()
+    {
+        
+    }
+
+    private void CheckDestructionState()
+    {
+        if (!_isFullyDestroyed)
+        {
+            foreach (var fuelTank in fuelTankObjects)
+            {
+                if (fuelTank.GetHasBeenDisabled())
+                {
+                    _isFullyDestroyed = true;
+                    _canReceiveRewards=false;
+                }
+            }
+
+            if (cargoHoldObject.GetHasBeenDisabled())
+            {
+                _canReceiveRewards = false;
+            }
+
+            if (computerRoomObject.GetHasBeenDisabled())
+            {
+                _isDisabled = true;
+            }
+        }
+    }
 }
